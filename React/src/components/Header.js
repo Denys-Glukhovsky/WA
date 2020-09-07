0804d70a5b0b0582a1feccea793fcc0e9f5c6401
Andrii.Zinchenko <Fiaroa4701a>
2020-09-07 09:09:17 +0300

    Fix

--- React/src/components/Header.js
+++ React/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route, Nav.Link} from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Container, Button, Form } from 'react-bootstrap'
 import logo from './logo.png';
@@ -21,34 +21,34 @@ class Header extends React.Component {
   render() {
     return (
       <>
-      <Navbar  collapseOnSelect expand='md' bg='dark' variant='dark'>
-        <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} height='35' width='192' className='d-inline-block align-top' alt='Logo'></img>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
-          <Navbar.Collapse id='responsive-navbar-nav'>
-            <Nav className='mr-auto'>
-              <Nav.Link href='/'>ГЛАВНАЯ</Nav.Link>
-              <Nav.Link href='/about'>ПРО НАС</Nav.Link>
-              <Nav.Link href='/for'>ДЛЯ КОГО</Nav.Link>
-              <Nav.Link href='/tarifs'>ТАРИФЫ</Nav.Link>
-              <Nav.Link href='/contacts'>КОНТАКТЫ</Nav.Link>
-            </Nav>
-            <Form inline>
-              <Button variant='outlined'>Войти</Button>
-              <Button variant='contained'>Регистрация</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
       <Router>
+        <Navbar  collapseOnSelect expand='md' bg='dark' variant='dark'>
+          <Container>
+            <Navbar.Brand href="/">
+              <img src={logo} height='35' width='192' className='d-inline-block align-top' alt='Logo'></img>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+            <Navbar.Collapse id='responsive-navbar-nav'>
+              <Nav className='mr-auto'>
+                <Link to='/'>ГЛАВНАЯ</Link>
+                <Link to='/about'>ПРО НАС</Link>
+                <Link to='/for'>ДЛЯ КОГО</Link>
+                <Link to='/tarifs'>ТАРИФЫ</Link>
+                <Link to='/contacts'>КОНТАКТЫ</Link>
+              </Nav>
+              <Form inline>
+                <Button variant='outlined'>Войти</Button>
+                <Button variant='contained'>Регистрация</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
         <Switch>
-          <Route exect path="/"  component={Home} />
-          <Route exect path="/about" component={About} />
-          <Route exect path="/for" component={For} />
-          <Route exect path="/tarifs" component={Tarifs} />
-          <Route exect path="/contacts" component={Contacts} />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/for" component={For} />
+          <Route path="/tarifs" component={Tarifs} />
+          <Route path="/contacts" component={Contacts} />
         </Switch>
       </Router>
       </>
